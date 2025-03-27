39d9e1c4b9d41ffd9aa30937c52ac303bc6e1f92
Max Maslov <mmiwf16@gmail.com>
2025-03-27 14:21:32 +0300

    hot fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 from typing import List
 import faiss
 import numpy as np
