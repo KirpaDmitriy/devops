88bd0783e0d2c49e5c2a19bc01ad89a427ad1dbf
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2025-04-10 12:01:18 +0300

    Create search.py

---
+++ search.py
@@ -0,0 +1,7 @@
+import numpy as np
+from db import index, documents
+
+def search(query_embedding: np.ndarray, k: int = 5):
+    distances, indices = index.search(query_embedding, k)
+    results = [(documents[i], distances[idx]) for idx, i in enumerate(indices[0])]
+    return results
