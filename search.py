564224cf07c370550b287b29185d2000e09d6ef3
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2025-04-10 12:38:46 +0300

    Update search.py

--- search.py
+++ search.py
@@ -3,5 +3,7 @@ from db import index, documents
 
 def search(query_embedding: np.ndarray, k: int = 5):
     distances, indices = index.search(query_embedding, k)
+    if not documents or not indices:
+        return []
     results = [(documents[i], distances[idx]) for idx, i in enumerate(indices[0])]
     return results
